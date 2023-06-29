80965a9729441967c6a72843a7ed1de05686449a
Joe Randazzo <jprandazzo@icloud.com>
2023-06-29 10:42:08 -0400

    test

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import Example from "./scripts/example"
 document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main")
     new Example(main)
+    console.log('test')
 })
