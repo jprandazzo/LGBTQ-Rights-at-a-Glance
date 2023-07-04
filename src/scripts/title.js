b2436e0b596c67c366e4ef258cd58b267a4df87b
Joe Randazzo <jprandazzo@icloud.com>
2023-07-04 19:07:13 -0400

    styling

--- src/scripts/title.js
+++ src/scripts/title.js
@@ -1,13 +1,14 @@
 class Title {
     constructor(ele) {
         this.ele = ele;
-        this.ele.innerHTML = "<h1>Status of LGBTQ+ <br>Issues by State</h1>";
-        this.ele.style.width = '500px';
-        this.ele.style.height = '200px';
+        this.ele.innerHTML = "<h1>LGBTQ+ Issues Across the US</h1>";
+        this.ele.style.width = 'auto';
+        this.ele.style.height = '100px';
         this.ele.style.display = 'flex';
-        this.ele.style.margin = '0 auto';
+        this.ele.style.justifyContent = 'space-around';
+        this.ele.style.padding = '0 0 20 0'
         this.ele.style.textAlign = 'center'
-        this.ele.style.fontSize = '30'
+        this.ele.style.fontSize = '26'
         this.handleClick = this.handleClick.bind(this);
 
         this.ele.addEventListener("click", this.handleClick);
