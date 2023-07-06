1de7528496cf5c068687aff3ae116e3eba0438d0
Joe Randazzo <131551196+jprandazzo@users.noreply.github.com>
2023-07-06 13:58:52 -0400

    Update title.js
    clean up code

--- src/scripts/title.js
+++ src/scripts/title.js
@@ -2,15 +2,17 @@ import List from './list'
 class Title {
     constructor(ele) {
         this.ele = ele;
+
         this.ele.innerHTML = "<h1>LGBTQ+ Rights Across the US</h1><br><br><br><span>No Pride For Some of Us Without Liberation For All of Us</span>";
+        //styling
         this.ele.style.width = 'auto';
         this.ele.style.height = '100px';
-        // this.ele.style.display = 'flex';
-        // this.ele.style.justifyContent = 'space-around';
         this.ele.style.padding = '0 0 30 0'
         this.ele.style.textAlign = 'center'
         this.ele.style.fontSize = '26'
 
+
+        //add instructions 
         let instructions = document.createElement('div')
         instructions.setAttribute('id','instructions')
         instructions.innerHTML = 'i'
@@ -22,6 +24,7 @@ class Title {
 
         this.ele.appendChild(instructions)
 
+        //add github link
         let github = document.createElement('div')
         github.setAttribute('id','github')
         github.innerHTML = '<a href="https://github.com/jprandazzo" target="_blank"><img src="https://aa-lgbtq-rights-map.s3.us-east-2.amazonaws.com/images/github-mark.svg"></a>'
@@ -29,6 +32,7 @@ class Title {
 
         this.ele.appendChild(github)
 
+        //add linkedin link
         let linkedin = document.createElement('div')
         linkedin.setAttribute('id','linkedin')
         linkedin.innerHTML = '<a href="https://linkedin.com/in/jprandazzo" target="_blank"><img src="https://aa-lgbtq-rights-map.s3.us-east-2.amazonaws.com/images/LI2.png"></a>'
