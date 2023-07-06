4dcc5e8e4a305e8594bef5ab74ed60e7e50d94bc
Joe Randazzo <131551196+jprandazzo@users.noreply.github.com>
2023-07-06 13:56:02 -0400

    Update issues_chart.js
    delete commented out code

--- src/scripts/issues_chart.js
+++ src/scripts/issues_chart.js
@@ -3,13 +3,4 @@ export default class Chart {
         this.obj = obj.node;
         this.name = obj.node.name;
         this.states = obj.node.states;
-    //     this.ele.innerHTML = "<h1>It's Alive!!!</h1>";
-    //     this.handleClick = this.handleClick.bind(this);
-
-    //     this.ele.addEventListener("click", this.handleClick);
-    }
-
-    // handleClick() {
-    //     this.ele.children[0].innerText = "Ouch!"
-    // }
 }
